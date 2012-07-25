d398e58cc9905c524340b83998c731605e56edf0
hysios <hysios@gmail.com>
2012-07-26 01:52:57 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChatRoom::Application.config.secret_token = 'ceff49fe3f39a8b058bb0753c3119d936bde67dc90b85f8f86cc87a8126974fe0967eeb9d83df5d32aa50420bcd5ff480fa6e2927e44b2ba5e6c3b0feace4ce3'
