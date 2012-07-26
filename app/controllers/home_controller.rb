a0ce1e506df6cf323ed791def1bfe048d333dd83
hysios hu <hysios@gmail.com>
2012-07-26 21:35:50 +0800

    final StreamServer session design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+  	session["hello"] = "world"
   end
 end
--- lib/streamserver.rb
+++ lib/streamserver.rb
@@ -7,15 +7,33 @@ class StreamServer < Sinatra::Base
 
   get '/stream', provides: 'text/event-stream' do
     stream :keep_open do |out|
-      settings.connections << out
-      out.callback { settings.connections.delete(out) }
-      out.errback { settings.connections.delete out }
+      conn = OpenStruct.new(:stream => out, :session_id => request.cookies.values[0])
+      settings.connections << conn
+      out.callback { settings.connections.delete(conn) }
+      out.errback { settings.connections.delete conn }
     end
   end
 
   post '/stream' do
     puts "Relay to #{settings.connections.count} streams"
-    settings.connections.each { |out| out << "data: #{params[:msg]}\n\n" }
+
+    settings.connections.each do |conn| 
+      conn.stream << "data: #{params[:msg]}\n\n" 
+    end
     204 # response without entity body
   end
+
+  def session_record(_sessoin_id = request.cookies.values[0])
+    ActiveRecord::SessionStore::Session.find_by_session_id(_sessoin_id)
+  end  
+
+  def current_user(_session = session)
+    user_key = _session["warden.user.user.key"]
+    Warden::SessionSerializer.new(User).deserialize(user_key)
+  end
+
+  def session(_sesison_record = session_record)
+    _sesison_record.data
+  end
+
 end
