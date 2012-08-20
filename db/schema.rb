ab1e916d1e13c9c235decedd00a6c53357e3d4ef
robin <binbinhuangcsu@gmail.com>
2012-08-20 15:46:54 +0800

    initalize the cancan

--- db/schema.rb
+++ db/schema.rb
@@ -19,19 +19,19 @@ ActiveRecord::Schema.define(:version => 20120731031009) do
   end
 
   create_table "room_members", :force => true do |t|
-    t.integer  "room_id"
-    t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "room_id",    :limit => 11
+    t.integer  "user_id",    :limit => 11
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
   end
 
   create_table "rooms", :force => true do |t|
     t.string   "name"
     t.string   "type"
-    t.integer  "member_limit"
-    t.integer  "onwer"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.integer  "member_limit", :limit => 11
+    t.integer  "onwer",        :limit => 11
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
   end
 
   create_table "sessions", :force => true do |t|
